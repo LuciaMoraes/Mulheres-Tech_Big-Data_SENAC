42a41e5240708f87edfc20c1831f322f64009d8e
LuciaMoraes <luciahfmoraes@gmail.com>
2024-09-20 21:27:55 -0300

    Arquivo do curso de Analista Big Data_Aula01
    Nesse projeto foram apresentados os códigos: INT, PRINT, FLOAT, INPUT.

---
+++ aula01.py
@@ -0,0 +1,39 @@
+print('ANÁLISE DE DADOS MULHERES TECH')    #TÍTULO DO CÓDIGO
+
+'''print('DIGITE UM NÚMERO:')    #código para o usuário digitar um número e retornar o dobro deste número.
+VALOR = int(input())      # CRIANDO UMA VARIÁVEL QUE VAI SOLICITAR O USUÁRIO A DIGITAR UM NÚMERO. O COMANDO INT É PARA FORÇAR O
+DOBRO = VALOR * 2
+print('O VALOR DO DOBRO É' , DOBRO)'''
+
+
+'''print('DIGITE A NOTA DO PRIMEIRO BIMESTRE')   #código para o usuário digitar as notas e retornar a média dos quatro bimestre dos alunos.
+B1 = float(input())
+print('DIGITE A NOTA DO SEGUNDO BIMESTRE')
+B2 = float(input())
+print('DIGITE A NOTA DO TERCEIRO BIMESTRE')
+B3 = float(input())
+print('DIGITE A NOTA DO QUARTO BIMESTRE')
+B4 = float(input())
+MEDIA = (B1 + B2 + B3 + B4) / 4
+print('A MÉDIA DO ALUNO É ', MEDIA)'''
+
+# código para o usuário inserir dois nº e retornar todas as operações básicas e o módulo.
+'''print('MULHERES TECH COPACABANA')
+
+print('DIGITE O PRIMEIRO NÚMERO')
+N1 = int(input())
+
+print('DIGITE O SEGUNDO NÚMERO')
+N2 = int(input())
+
+SOMA = N1 + N2
+SUBTRACAO = N1 - N2
+MULTIPLICACAO = N1 * N2
+DIVISAO = N1 / N2
+MODULO = N1 % N2
+
+print('O RESULTADO DA SOMA É: ' , SOMA)
+print('O RESULTADO DA SUBTRACAO É: ' , SUBTRACAO)
+print('O RESULTADO DA MULTIPLICACAO É: ' , MULTIPLICACAO)
+print('O RESULTADO DA DIVISAO É: ' , DIVISAO)
+print('O RESULTADO DO MODULO É: ' , MODULO)'''
