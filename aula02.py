1ee30dbbdcd9570a4153d62774a6c97018492161
LuciaMoraes <luciahfmoraes@gmail.com>
2024-09-20 21:33:53 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,78 @@
+'''print('Qual a sua idade?')
+idade = int(input())
+
+if idade >= 18:
+    print('Acesso liberado! Boa Festa!')
+else:
+    print('Acesso negado! Você é menor de idade')'''
+
+    #CÓDIGO PARA LIBERAR ACESSO APENAS PARA MAIOES E 18 ANOS
+
+'''print('Qual a sua idade?')
+idade = int(input())
+
+if idade < 18:
+    print('Acesso Negado! Você é menor de idade!')
+elif idade == 18:
+    print('Você está quase lá! Mais um ano e você terá acesso!')
+else:
+    print('Acesso Liberado! Boa Festa!')'''
+
+#código para verificar se o aluno está aprovado.
+'''print('Digite a nota do primeiro bimestre')
+B1 = float(input())
+print('Digite a nota do segundo bimestre')
+B2 = float(input())
+print('Digite a nota do terceiro bimestre')
+B3 = float(input())
+print('Digite a nota do quarto bimestre')
+B4 = float(input())
+media = (B1 + B2 + B3 + B4) / 4
+print('A média do aluno é: ' , media)
+
+if media >= 7:
+    print('APROVADO!')
+elif media >= 5:
+    print('RECUPERAÇÃO!')
+else:
+    print('REPROVADO!')'''
+
+#CÓDIGO PARA SABER SE PODE PARTICIPAR DO MULHERES TECH
+
+'''print('Qual seu gênero? (F ou M)')
+genero = input()
+print('Qual município você mora?')
+municipio = input()
+
+if genero.upper() == 'F' and municipio.lower() == 'rio de janeiro':
+    print('Você está classificada a participar do Mulheres tech!')
+else:
+    print('Infelizmente, você não pode participar do Mulheres Tech!')'''
+
+#código para saber se o acesso a um filme será permitido de acordo com a classificação etária.
+
+'''print("*" * 30 , 'BEM VINDO AO CINEMA SEVERIANO RIBEIRO', "*" * 30)
+print(' ')
+print('Qual a sua idade?')
+idade = int(input())
+
+if idade >= 18:
+    print('Acesso liberado!\n Bom Filme!\nAproveite nosso combo de pipoca com refri de 700ml!')
+
+elif idade >= 16:
+    print('O filme selecionado é para maiores de 18 anos\npara assistir você precisa estar acompanhado de um responsável.\nVocê está acompanhado de um responsável?')
+    responsavel = input().upper()
+    if responsavel == 'SIM':
+            print('Acesso Liberado!\nBom Filme!\nAproveite nosso combo de pipoca!')
+
+    else:
+        print('Você só pode ver o filme acompanhado de um responsável!')
+
+else:
+    print('Acesso Negado!\nVocê é menor de idade!')'''
+
+
+
+
+
+    
